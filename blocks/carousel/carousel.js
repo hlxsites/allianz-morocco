98e44986ff9369b5851c5284087f064097a1ab63
Mike Tilburg <mtilburg@adobe.com>
2023-10-30 20:20:40 +0100

    simple previous and next for slides

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,3 +1,40 @@
+function nextSlide() {
+  const carouselButtons = document.querySelector('.carousel-buttons');
+
+  for( let i = 0; i < carouselButtons.children.length; i++) {
+    if (carouselButtons.children[i].classList.contains('selected')) {
+      if( carouselButtons.children[i].nextElementSibling != null) {
+        carouselButtons.children[i].nextElementSibling.click();
+        return;
+      } else {
+        carouselButtons.children[0].click();
+        return;
+      }
+    }
+  }
+}
+
+function prevSlide() {
+  let carouselButtons = document.querySelector('.carousel-buttons');
+
+  for( let i = carouselButtons.children.length-1; i >= 0; i--) {
+    //console.log(i);
+    if (carouselButtons.children[i].classList.contains('selected')) {
+      if( i != 0 ) {
+        carouselButtons.children[i].previousElementSibling.click();
+        return;
+      } else {
+        carouselButtons.children[carouselButtons.children.length-1].click();
+        return;
+      }
+    }
+  }
+}
+
+function start() {
+    setInterval(() => nextSlide(), 5000);
+}
+
 export default function decorate(block) {
   const buttons = document.createElement('div');
   buttons.className = 'carousel-buttons';
@@ -18,4 +55,6 @@ export default function decorate(block) {
     buttons.append(button);
   });
   block.parentElement.append(buttons);
+
+  start();
 }
